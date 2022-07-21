256ee126b508089b0d72db16c3bcf1184b6c1c49
kjpranata <kevjepe@gmail.com>
2022-07-21 20:42:00 +0800

    first

---
+++ Controllers/ConstructorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CASS___Construction_Assistance.Controllers
+{
+    public class ConstructorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
