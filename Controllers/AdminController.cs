7ba929b33b16da96c121ddfaa8216066f81f9d8b
howard halim <halimhoward@gmail.com>
2022-07-23 04:50:13 +0800

    admin+customer

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CASS___Construction_Assistance.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Customer()
+        {
+            return View();
+        }
+        public IActionResult Constructor()
+        {
+            return View();
+        }
+    }
+}
