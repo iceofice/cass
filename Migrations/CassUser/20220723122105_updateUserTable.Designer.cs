8e998a1b8e25a68e1f6076652f97197ee7c2bb4f
Renaldy Prasetio <renireas@gmail.com>
2022-07-23 20:24:01 +0800

    Update database

--- Migrations/CassUser/20220723122105_updateUserTable.Designer.cs
+++ Migrations/CassUser/20220723122105_updateUserTable.Designer.cs
@@ -4,14 +4,16 @@ using CASS___Construction_Assistance.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CASS___Construction_Assistance.Migrations.CassUser
 {
     [DbContext(typeof(CassUserContext))]
-    partial class CassUserContextModelSnapshot : ModelSnapshot
+    [Migration("20220723122105_updateUserTable")]
+    partial class updateUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
