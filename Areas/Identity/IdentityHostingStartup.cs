49d58f21fa0b6cee0750db8f3dec2067b7cec574
Renaldy Prasetio <renireas@gmail.com>
2022-07-23 18:55:13 +0800

    Revert

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CASS___Construction_Assistance.Areas.Identity.Data;
+using CASS___Construction_Assistance.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CASS___Construction_Assistance.Areas.Identity.IdentityHostingStartup))]
+namespace CASS___Construction_Assistance.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CassUserContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CassUserContextConnection")));
+
+                services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<CassUserContext>();
+            });
+        }
+    }
+}
