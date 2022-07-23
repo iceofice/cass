aafcf80d36d50e0e00aa397eb10f60e082e6f37e
Steven <57295193+sterbeN27@users.noreply.github.com>
2022-07-23 18:28:38 +0800

    update admin dashboard, logo, img

---
+++ Controllers/AdminUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CASS___Construction_Assistance.Controllers
+{
+    public class AdminUser : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
